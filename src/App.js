78ea5370d1445766fcde87fb8512a0b2ac836bf1
“Joel <joelsigel@gmail.com>
2019-03-23 16:11:06 -0700

    initial build

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'reactstrap';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRectangleWide } from '@fortawesome/pro-light-svg-icons/faRectangleWide';
+library.add(faRectangleWide)
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Row>
+          <Col xs="2">&lt;/J03L&gt;</Col>
+          <Col xs="8"></Col>
+          <Col xs="2" align="right"><FontAwesomeIcon icon={faRectangleWide} size="2x" /></Col>
+        </Row>
+      </Container>
     );
   }
 }
