80605bdaaef8a2daa412dcf9af56b18c1fbc0ab0
“Joel <joelsigel@gmail.com>
2019-03-23 18:19:23 -0700

    Nav adjustments

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap';
+import './Nav.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRectangleWide } from '@fortawesome/pro-light-svg-icons/faRectangleWide';
@@ -9,11 +10,13 @@ class Nav extends Component {
 
   render(){
     return [
-      <Container>
+      <Container className="navigation">
         <Row>
-          <Col xs="2">&lt;/J03L&gt;</Col>
+          <Col className="logo" xs="2">&lt;/J03L&gt;</Col>
           <Col xs="8"></Col>
-          <Col xs="2" align="right"><FontAwesomeIcon icon={faRectangleWide} size="2x" /></Col>
+          <Col xs="2" className="menu" align="right">
+            <FontAwesomeIcon icon={faRectangleWide} size="2x" />
+          </Col>
         </Row>
       </Container>
     ]
