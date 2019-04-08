2592dd27373f54a17be88fcd9598253bfb87fe59
“Joel <joelsigel@gmail.com>
2019-04-08 06:56:16 -0700

    Build update

--- src/Footer.js
+++ src/Footer.js
@@ -10,10 +10,10 @@ class Footer extends Component {
         <Container>
           <Row>
             <Col xs="12">
-              <p className="subtitle">Something | <span className="text-muted">Something</span></p>
+              <p className="subtitle">Something | <span className="text-muted">03/25/2019</span></p>
               <h2>Something</h2>
-              <p>Something with something else might make something more.</p>
-              <Button color="white">Danger! </Button>
+              <p className='description'>Sometimes somethings surprise someone so stupidly, steven scientifically switches subliminal sides.</p>
+              <Button color="white">Something!</Button>
             </Col>
           </Row>
         </Container>
--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import './Video.css';
+import video from './test.mp4';
 
 class Video extends Component {
 
   render(){
     return [
-      <video autoPlay loop src="http://joelsigel.com/mock-404/assets/videos/342932613.mp4"></video>
+      <video autoPlay loop muted src={video}></video>
+      // <video autoPlay loop muted src="http://joelsigel.com/mock-404/assets/videos/342932613.mp4"></video>
     ]
   }
 }
